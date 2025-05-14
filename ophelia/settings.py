721363bfc577f541904199da81715e44e2056e98
DanielE-J <biz45dy2@students.codeinstitute.net>
2025-05-14 20:16:08 +0200

    prepare for deploy

--- ophelia/settings.py
+++ ophelia/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ophelia-resturant.herokuapp.com', 'localhost']
 
 
 # Application definition
