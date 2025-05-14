ea53fd04f97d2e2a82bc08cc7c614b4b9ddd957c
DanielE-J <biz45dy2@students.codeinstitute.net>
2025-05-14 20:06:08 +0200

    added secret key

--- ophelia/settings.py
+++ ophelia/settings.py
@@ -23,11 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1_m&m+jyoall=#gd@d^-cx7&7e6hzhffiel#$^vs0fw$pob43m'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
