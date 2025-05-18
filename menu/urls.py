e5cbbb8141d3497d5199db7ca4507272210d5b83
DanielE-J <biz45dy2@students.codeinstitute.net>
2025-05-18 20:44:55 +0200

    Added menu app

---
+++ menu/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.all_menus, name='all_menus'),
+    path('add/', views.add_menu, name='add_menu'),
+    path('update/<int:menu_id>/', views.update_menu, name='update_menu'),
+    path('delete/<int:menu_id>/', views.delete_menu, name='delete_menu'),
+]
--- ophelia/settings.py
+++ ophelia/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'cloudinary',
     'home',
+    'menu',
+    
 ]
 
 SITE_ID = 1
