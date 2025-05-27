bfd271d7b155bd1a528741dc3dbf031e4009f44d
DanielE-J <biz45dy2@students.codeinstitute.net>
2025-05-27 11:44:35 +0200

    Added env.py and settings

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_yqKnlex27vYz@ep-tight-rice-a20s0upu.eu-central-1.aws.neon.tech/click_tusk_saint_81889")
--- ophelia/settings.py
+++ ophelia/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-z9lf5#ov^c(d+-p4^g4%k2e80u#t&6i5)1yl+8gt94p3nysxe+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -78,13 +78,16 @@ WSGI_APPLICATION = 'ophelia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
