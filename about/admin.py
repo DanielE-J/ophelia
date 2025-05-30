7fdf6e7c1cfcfff800763fc3a4a09d99921ccabb
DanielE-J <biz45dy2@students.codeinstitute.net>
2025-05-30 09:16:21 +0200

    Added more to about to make it work

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import About
+
+
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+    """
+    Customize the django admin interface for the About model.
+
+    This class customizes the admin interface for the about model by using the
+    summernote editor for the content field.
+
+    Attributes:
+    summernote_fields (tuple): Fields that should use the summernote richtext
+    editor which is the 'content' field.
+    """
+    summernote_fields = ('content',)
--- ophelia/urls.py
+++ ophelia/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),       
     path('menu/', include('menu.urls')),
-    path('accounts/', include('allauth.urls')),  
+    path('accounts/', include('allauth.urls')),
+    path('about/', include("about.urls"), name="about-urls"),  
 ]
