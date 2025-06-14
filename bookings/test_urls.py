f4f508c89e9c98b93bf0f7640cbcd5445702e260
DanielE-J <biz45dy2@students.codeinstitute.net>
2025-06-14 23:04:42 +0200

    added auto test

--- bookings/test_urls.py
+++ bookings/test_urls.py
@@ -1,12 +1,6 @@
-# Imports
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 3rd party:
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Internal:
-from bookings.views import (Reservations, BookingList,
-                            Confirmed, EditBooking, cancel_booking)
+from bookings.views import Reservations, Confirmed, BookingList, EditBooking, cancel_booking
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -15,26 +9,25 @@ from bookings.views import (Reservations, BookingList,
 
 class TestReservationsUrls(SimpleTestCase):
     """
-    This class is for testing the bookings app
-    urls
+    This class is for testing the bookings app URLs.
     """
 
     def test_reservations_resolved(self):
         url = reverse('reservations')
-        self.assertEquals(resolve(url).func.view_class, Reservations)
+        self.assertEqual(resolve(url).func.view_class, Reservations)
 
     def test_confirmed_resolved(self):
         url = reverse('confirmed')
-        self.assertEquals(resolve(url).func.view_class, Confirmed)
+        self.assertEqual(resolve(url).func.view_class, Confirmed)
 
     def test_booking_list_resolved(self):
         url = reverse('booking_list')
-        self.assertEquals(resolve(url).func.view_class, BookingList)
+        self.assertEqual(resolve(url).func.view_class, BookingList)
 
     def test_edit_booking_url_resolved(self):
         url = reverse('edit_booking', args=['1'])
-        self.assertEquals(resolve(url).func.view_class, EditBooking)
+        self.assertEqual(resolve(url).func.view_class, EditBooking)
 
     def test_delete_booking_url_resolved(self):
         url = reverse('cancel_booking', args=['1'])
-        self.assertEquals(resolve(url).func, cancel_booking)
+        self.assertEqual(resolve(url).func, cancel_booking)
