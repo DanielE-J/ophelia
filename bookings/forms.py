7a75dd3e330a9b7d1439bc60cba7151b7f7a017f
DanielE-J <biz45dy2@students.codeinstitute.net>
2025-06-15 11:58:40 +0200

    went back

--- bookings/forms.py
+++ bookings/forms.py
@@ -2,8 +2,6 @@ from django import forms
 from datetime import datetime
 from phonenumber_field.formfields import PhoneNumberField
 from .models import Booking, time_slots, Table
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 
 class BookingForm(forms.ModelForm):
     requested_date = forms.DateField(
@@ -76,31 +74,4 @@ class BookingForm(forms.ModelForm):
                     f"Sorry, {time} on {date} is fully booked. Please choose a different time."
                 )
         return cleaned_data
-    
-
-class CustomSignUpForm(UserCreationForm):
-    email = forms.EmailField(required=False)
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'Username'
-        })
-        self.fields['email'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'Email address'
-        })
-        self.fields['password1'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'Password'
-        })
-        self.fields['password2'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'Password (again)'
-        })
     
